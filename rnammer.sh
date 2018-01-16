3f24b1c82b23058f0c15cc3c2916f88976d43bc9
Urwah Nawaz <30397183+unawaz1996@users.noreply.github.com>
2018-01-16 14:12:38 +1030

    Add files via upload

---
+++ rnammer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+AssemblyDIR==/home/urwah/Trinity/Assembly/all_trinity/Trinotate/
+
+
+$TRINOTATE_HOME/util/rnammer_support/RnammerTranscriptome.pl --transcriptome {$AssemblyDIR}/Trinity.fasta --path_to_rnammer /home/urwah/rnammer-1.2
