4514ae73ffacbe3fe7f1469499b51e15126b1dac
Urwah Nawaz <30397183+unawaz1996@users.noreply.github.com>
2018-04-28 22:57:33 +0930

    Update extractUnnannotated.py

--- Unannotated/extractUnnannotated.py
+++ Unannotated/extractUnnannotated.py
@@ -14,3 +14,4 @@ unannotated.to_csv('trinotate_unannotated.csv')
 
 # everything above can be ran as a oneliner code 
 #pd.read_csv("trinotate_report.csv")[(dataframe.sprot_Top_BLASTP_hit == ".") & (dataframe.sprot_Top_BLASTX_hit == ".") & (dataframe.prot_id == ".")].to_csv('output_unannotated.csv')
+#cat unannotated.txt | cut -d ',' -f3 > unannotatedTranscripts.txt
