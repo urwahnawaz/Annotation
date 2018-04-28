047c43412ab970b789d63d63f06fbeaad1454b34
Urwah Nawaz <30397183+unawaz1996@users.noreply.github.com>
2018-04-28 22:27:02 +0930

    Create extractUnnannotated.py

---
+++ Unannotated/extractUnnannotated.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+annotationreport = "trinotate_report.csv"
+dataframe = pd.read_csv(annotationreport)
+
+blastp = (dataframe.sprot_Top_BLASTP_hit == ".")
+blastx = (dataframe.sprot_Top_BLASTX_hit == ".")
+protein = (dataframe.prot_id == ".")
+
+unannotated = dataframe[blastp & blastx & protein]
+
+unannotated.to_csv('trinotate_unannotated.csv')
+
+# everything above can be ran as a oneliner code 
+#pd.read_csv("trinotate_report.csv")[(dataframe.sprot_Top_BLASTP_hit == ".") & (dataframe.sprot_Top_BLASTX_hit == ".") & (dataframe.prot_id == ".")].to_csv('output_unannotated.csv')
