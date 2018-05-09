04347428a2584eec87ef4c484fe0cdaa7df84129
Urwah Nawaz <30397183+unawaz1996@users.noreply.github.com>
2018-05-09 17:16:25 +0930

    Create transdecoder_predict.sh

---
+++ transdecoder_predict.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -p batch
+#SBATCH -n 12
+#SBATCH --time=08:00:00
+#SBATCH --mem=50GB
+
+# notification configuration
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=urwah.nawaz@student.adelaide.edu.au
+
+
+
+##module load Trinity/2.5.1-foss-2016b
+##module load BLAST+/2.6.0-foss-2016b-Python-2.7.13
+module load  Perl/5.24.0-foss-2017a
+
+
+
+//fast/users/a1654797/TransDecoder/TransDecoder-TransDecoder-v5.0.2/TransDecoder.Predict -t //fast/users/a1654797/Second_try/Trinity_assembly/Trimming_withTGtry1/trinity_out_dir/Trinity.fasta
